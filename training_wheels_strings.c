2926214e802c63431da55e8acb64bf62a1ec3f73
Harzinn <nickkrugz@gmail.com>
2024-07-11 18:29:03 +0000

    week 4 memory, first break

---
+++ training_wheels_strings.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// Removing cs50.h training wheels for strings!!!
+// #include <cs50.h>
+
+// What training wheels were actually doing
+// typedef char *string;
+
+int main(void) {
+    // Training wheels way
+    // string s = "HI!";
+    // printf("%s\n", s);  
+ 
+    // Pointer arithmetic to get values of the "string" s, or the array of chars
+    char *s = "HI!";
+    printf("%c", *s);
+    printf("%c", *(s + 1));
+    printf("%c\n", *(s + 2));
+
+    // Same as above, but with array indexing!
+    printf("%c", s[0]);
+    printf("%c", s[1]);
+    printf("%c\n", s[2]);
+}
