4b3d3990588a4523f2e6a8458c3b4e60f529edc6
Harzinn <nickkrugz@gmail.com>
2024-07-13 15:06:18 -0400

    updated hi with pointers

--- hi.c
+++ hi.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-#include <cs50.h>
 
 int main(void)
 {
+    char name[100];
 
-    string words[2];
+    printf("Enter your name: ");
+    scanf("%99s", name);
 
-    words[0] = "HI!";
-    words[1] = "BYE!";
-
-    printf("%s\n", words[0]);
-    printf("%s\n", words[1]);
+    printf("Hello, %s!\n", name);
+    printf("%p!\n", (void *)name);
 }
